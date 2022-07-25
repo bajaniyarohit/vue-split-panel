5a006cd03909aacf4f3e0122f77e549e3e0912c2
nsano-rururu <nsano@ae.em-net.ne.jp>
2022-07-26 00:01:42 +0900

    webpack 4 to 5

--- build/webpack.base.config.js
+++ build/webpack.base.config.js
@@ -14,20 +14,24 @@ module.exports = {
     rules: [{
         // https://vue-loader.vuejs.org/en/configurations/extract-css.html
         test: /\.vue$/,
-        loader: 'vue-loader',
-        options: {
-          loaders: {
-            css: 'vue-style-loader!css-loader',
-            less: 'vue-style-loader!css-loader!less-loader'
-          },
-          postLoaders: {
-            html: 'babel-loader'
+        use: [
+          {
+            loader: 'vue-loader',
+            options: {
+              loaders: {
+                css: 'vue-style-loader!css-loader',
+                less: 'vue-style-loader!css-loader!less-loader'
+              },
+              postLoaders: {
+                html: 'babel-loader'
+              }
+            }    
           }
-        }
+        ]
       },
       {
         test: /\.js$/,
-        loader: 'babel-loader',
+        use: ['babel-loader'],
         exclude: /node_modules/
       },
       {
@@ -54,7 +58,7 @@ module.exports = {
           'sass-loader?sourceMap'
         ]
       },
-      { test: /\.(gif|jpg|png|woff|svg|eot|ttf)\??.*$/, loader: 'url-loader?limit=8192' },
+      { test: /\.(gif|jpg|png|woff|svg|eot|ttf)\??.*$/, use: ['url-loader?limit=8192'] },
       { test: /\.(html|tpl)$/, loader: 'html-loader' }
     ]
   },
